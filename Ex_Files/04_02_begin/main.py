d46933d965c6bf24588121e2d3d0e4d19596a988
Matheus Marques Albuquerque <80928621+matheuscmf@users.noreply.github.com>
2023-01-11 15:19:42 +0000

    modified:   main.py

--- Ex_Files/04_02_begin/main.py
+++ Ex_Files/04_02_begin/main.py
@@ -1,3 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "Olá Navegantes,vamos estudar esta bela linguagem que é Python!!!!!"
+
+app.run(debug=True)
